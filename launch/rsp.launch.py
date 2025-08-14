5e29db8f85ec4d921afdda5ab6b0083bcdb25ea4
troyfider <gerrardtroyfider@gmail.com>
2025-08-14 14:57:29 +0800

    Changed codes to ROS1 compatible

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -1,41 +1,16 @@
-import os
+<launch>
+  <!-- Argument: use_sim_time -->
+  <arg name="use_sim_time" default="false" />
 
-from ament_index_python.packages import get_package_share_directory
+  <!-- Load the robot_description parameter from the Xacro file -->
+  <param name="robot_description"
+         command="$(find xacro)/xacro '$(find my_bot)/description/robot.urdf.xacro'" />
 
-from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
-from launch.actions import DeclareLaunchArgument
-from launch_ros.actions import Node
-
-import xacro
-
-
-def generate_launch_description():
-
-    # Check if we're told to use sim time
-    use_sim_time = LaunchConfiguration('use_sim_time')
-
-    # Process the URDF file
-    pkg_path = os.path.join(get_package_share_directory('my_bot'))
-    xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
-    robot_description_config = xacro.process_file(xacro_file)
-    
-    # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
-    node_robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='screen',
-        parameters=[params]
-    )
-
-
-    # Launch!
-    return LaunchDescription([
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='false',
-            description='Use sim time if true'),
-
-        node_robot_state_publisher
-    ])
+  <!-- Start the robot_state_publisher node -->
+  <node pkg="robot_state_publisher"
+        type="robot_state_publisher"
+        name="robot_state_publisher"
+        output="screen">
+    <param name="use_sim_time" value="$(arg use_sim_time)" />
+  </node>
+</launch>
